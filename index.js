19f3b806d9df851744bb2a6d5f8ff8679f99f5e5
josué <besteujt@gmail.com>
2019-07-02 18:56:12 +0100

    example code

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const users = [
+  { id: 0, name: 'Pepe' }, 
+  { id: 1, name: 'Juan' }
+];
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.get('/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const user = users.find(user => user.id === userId);
+  if(user) res.json(user); else res.send('Usuario no encontrado');
+});
+
+app.get('/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const user = users.find(user => user.id === userId);
+  if(user) res.json(user); else res.send('Usuario no encontrado');
+});
+
 
 app.listen(3000, () => console.log('Ready on port 3000!'));
